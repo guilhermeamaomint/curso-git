138221dd59657872567afcfe9188d8177140c0aa
guilhermegit <g360guilherme@gmail.com>
2023-03-21 17:22:35 -0300

    alteração no codigo

--- codigo.py
+++ codigo.py
@@ -1,4 +1,7 @@
-print("boa tarde, estou a prendendo a usar o git")
-#    :)
-x = 1 
-print("o valor de x é:", x)
+import random as r
+
+print("boa tarde, estou aprendendo a usar o git")
+
+x = r.randint(1, 100)
+
+print("o valor de x é:", str(x))
